ff7197ab390147e01f7cb94b8f379edd5389d356
ISSOtm <eldredhabert0@gmail.com>
2021-09-04 16:12:06 +0200

    Fix compile errors with RGBDS 0.5.0+

--- Variables.asm
+++ Variables.asm
@@ -11,25 +11,25 @@ SECTION	"Variables",WRAM0
 ; Global variables
 ; ================================================================
 
-GBCFlag				ds	1
-sys_btnHold			ds	1	; held buttons
-sys_btnPress		ds	1	; pressed buttons
+GBCFlag:			ds	1
+sys_btnHold:		ds	1	; held buttons
+sys_btnPress:		ds	1	; pressed buttons
 
-CurrentSample		ds	1
+CurrentSample:		ds	1
 
 ; ================================================================
 
 SECTION "Temporary register storage space",HRAM
 
-tempAF				ds	2
-tempBC				ds	2
-tempDE				ds	2
-tempHL				ds	2
-tempSP				ds	2
-tempPC				ds	2
-tempIF				ds	1
-tempIE				ds	1
-OAM_DMA				ds	8
+tempAF:				ds	2
+tempBC:				ds	2
+tempDE:				ds	2
+tempHL:				ds	2
+tempSP:				ds	2
+tempPC:				ds	2
+tempIF:				ds	1
+tempIE:				ds	1
+OAM_DMA:			ds	8
 
 ; ================================================================
 
